1ec87dab42f2ba213e8d8d71f31543f0f44af3bf
Jason Ola <olajason@yahoo.fr>
2022-05-16 14:21:42 +0200

    tests

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+SUN_RADIUS = 1392/2
+
 d3.csv("planets.csv").then(donnees => {
     console.log(donnees)
+    for(let i = 0; i<donnees.length; i++){
+        console.log(donnees[i].planet, donnees[i].diameter)
+    }
+    let scene = document.querySelector("a-scene")
+    let sun = document.createElement("a-sphere")
+    sun.setAttribute("radius",SUN_RADIUS)
+    sun.setAttribute("position","0 0 -10")
+    scene.appendChild(sun)
+    let mercury = document.createElement("a-sphere")
+    mercury.setAttribute("radius", donnees[0].diameter/1000)
+    mercury.setAttribute("position",`${SUN_RADIUS+donnees[0]} 0 -10`)
+    console.log(mercury)
+    scene.appendChild(mercury)
 })
