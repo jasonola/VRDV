a4ab17dc65eff5bc355638bbf7b092e32d67581b
Jason Ola <olajason@yahoo.fr>
2022-05-17 10:04:12 +0200

    for loop for all planets

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ d3.csv("planets.csv").then(donnees => {
     }
     let scene = document.querySelector("a-scene")
     let sun = document.createElement("a-sphere")
+    
     sun.setAttribute("radius",SUN_RADIUS)
     sun.setAttribute("position","0 0 -10")
+    sun.setAttribute("color", "orange")
     scene.appendChild(sun)
-    let mercury = document.createElement("a-sphere")
-    mercury.setAttribute("radius", donnees[0].diameter/1000)
-    mercury.setAttribute("position",`${SUN_RADIUS+donnees[0]} 0 -10`)
-    console.log(mercury)
-    scene.appendChild(mercury)
+
+    for (let i = 0; i < donnees.length; i++) {
+        let planet = document.createElement("a-sphere")
+        planet.setAttribute("radius", donnees[i].diameter/1000)
+        planet.setAttribute("position",`${SUN_RADIUS+ +donnees[i].distance_from_sun} 0 -10`)
+        scene.appendChild(planet)
+    }
+    // let mercury = document.createElement("a-sphere")
+    // mercury.setAttribute("radius", donnees[0].diameter/1000)
+    // mercury.setAttribute("position",`${SUN_RADIUS+ +donnees[0].distance_from_sun} 0 -10`)
+    // console.log(SUN_RADIUS)
+    // console.log(`${SUN_RADIUS+ +donnees[0].distance_from_sun} 0 -10`)
+    // scene.appendChild(mercury)
 })
