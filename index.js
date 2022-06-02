29198d173cb0d61aaa43f1ef1434594e78cc6798
Jason Ola <olajason@yahoo.fr>
2022-06-02 10:19:38 +0200

    add cursor

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ d3.csv("planets.csv",function(d){
     // Selection de la scène pour ensuite y ajouter les differents éléments
     let scene = document.querySelector("a-scene")
     // Création du soleil et de la caméra
-    let sun = document.createElement("a-sphere")
-    let camera = document.querySelector("a-camera")
-    
+    let sun = document.createElement("a-sphere")    
     
     // Définir les caracteristiques du soleil et l'ajouter à la scène
     sun.setAttribute("radius",SUN_RADIUS_KM*10)
@@ -44,6 +42,8 @@ d3.csv("planets.csv",function(d){
         planet.setAttribute("radius", +donnees[i].radius_km*50)
         planet.setAttribute("position",`${SUN_RADIUS_KM*10+ +donnees[i].sun_dist_km/100} 0 -10`)
         planet.setAttribute("id", donnees[i].planet)
+        planet.setAttribute("number", i)
+
         // Apposer les textures correspondantes
         planet.setAttribute("src", ASSETS + PLANET_TEXTURES[i])
         // C'est sur planet qu'on définit la vitesse de rotation
@@ -55,6 +55,23 @@ d3.csv("planets.csv",function(d){
         scene.appendChild(center)
         center.appendChild(planet)
         planet.appendChild(text)
+        console.log(planet)
     }
-   
+
+    let infoWrapper = document.getElementById("planet_info")
+    let title = document.createElement("h3")
+    console.log(donnees[0])
+
+
+    // Ajouter tableau planete sur le tableau de bord du cockpit
+    // let tableWrapper = document.getElementById("planet_info")
+    // let table = document.createElement('table');
+    // let thead = document.createElement('thead');
+    // let tbody = document.createElement('tbody');
+
+    // table.appendChild(thead);
+    // table.appendChild(tbody);
+    // tableWrapper.appendChild(table)
+    // console.log(donnees)
+    
 })
